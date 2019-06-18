aef678d8f4a9f9313ed5caebfc464132718b3583
Muhammed Karakul <muhammedkarakul@gmail.com>
2019-06-18 18:09:37 +0300

    Projede kullanılacak Firma, Is, Personel, Belge, BelgeDurum ve Eposta modelleri oluşturuldu ve veri tabanına eklendi.

--- safeputapp/admin.py
+++ safeputapp/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Firma, Is, Personel, Belge, BelgeDurum, Eposta
 
 # Register your models here.
+admin.site.register(Firma)
+admin.site.register(Is)
+admin.site.register(Personel)
+admin.site.register(Belge)
+admin.site.register(BelgeDurum)
+admin.site.register(Eposta)
--- safeputapp/models.py
+++ safeputapp/models.py
@@ -7,53 +7,75 @@ from django.db import models
 
 # Firma nesnesini tanımlayan model.
 class Firma(models.Model):
-    kutuk_no = models.CharField(max_lenght = 9, verbose_name = "Kütük Numarası")
-    unvan = models.CharField(max_lenght = 50, verbose_name = "Ünvan")
-    adres = models.CharField(max_lenght = 250, verbose_name = "Adres")
-    posta_kodu = models.CharField(max_lenght = 5, verbose_name = "Posta Kodu")
-    sehir = models.CharField(max_lenght = 20, verbose_name = "Şehir")
-    telefon = models.CharField(max_lenght = 11, verbose_name = "Telefon")
-    faks = models.CharField(max_lenght = 11, verbose_name = "Faks")
-    eposta = models.CharField(max_lenght = 50, verbose_name = "E-Posta")
+    kutuk_no = models.CharField(max_length = 10, verbose_name = "Kütük Numarası")
+    unvan = models.CharField(max_length = 50, verbose_name = "Ünvan")
+    adres = models.CharField(max_length = 250, verbose_name = "Adres")
+    posta_kodu = models.CharField(max_length = 10, verbose_name = "Posta Kodu")
+    sehir = models.CharField(max_length = 50, verbose_name = "Şehir")
+    telefon = models.CharField(max_length = 20, verbose_name = "Telefon")
+    faks = models.CharField(max_length = 20, verbose_name = "Faks")
+    eposta = models.CharField(max_length = 50, verbose_name = "E-Posta")
     aktifmi = models.BooleanField(verbose_name = "Aktif mi?", default = True)
-    olusturma_tarihi = models.DateTimeField(verbose_name = "Oluşturma Tarihi")
+    olusturma_tarihi = models.DateTimeField(auto_now_add = True, blank = True, verbose_name = "Oluşturma Tarihi")
     duzunleme_tarihi = models.DateTimeField(verbose_name = "Duzenleme Tarihi", blank = True, null = True)
 
 
 # İş nesnesini tanımlayan model.
 class Is(models.Model):
-    tanim = models.CharField(max_lenght = 250, verbose_name = "Tanım")
-    fatura_no = models.CharField(max_lenght = 20, verbose_name = "Fatura Numarası")
-    guvenlik_seviye = models.CharField(max_lenght = 20, verbose_name = "Güvenlik Seviyesi")
-    is_durum = models.CharField(max_lenght = 20, verbose_name = "İş Durumu")
+    tanim = models.CharField(max_length = 250, verbose_name = "Tanım")
+    fatura_no = models.CharField(max_length = 20, verbose_name = "Fatura Numarası")
+    guvenlik_seviye = models.CharField(max_length = 20, verbose_name = "Güvenlik Seviyesi")
+    is_onay = models.BooleanField(verbose_name = "İş Onayı", default = False)
     aktifmi = models.BooleanField(verbose_name = "Aktif mi?", default = True)
-    firma_id = models.CharField(max_lenght = 10, verbose_name = "Firma ID")
-    olusturma_tarihi = models.DateTimeField(verbose_name = "Oluşturma Tarihi")
+    firma_id = models.ForeignKey(Firma, on_delete = models.CASCADE, verbose_name = "Firma")
+    olusturma_tarihi = models.DateTimeField(auto_now_add = True, blank = True, verbose_name = "Oluşturma Tarihi")
     duzunleme_tarihi = models.DateTimeField(verbose_name = "Duzenleme Tarihi", blank = True, null = True)
 
 
 # Personel nesnesini tanımlayan model.
 class Personel(models.Model):
-    ad = models.CharField(max_lenght = 50, verbose_name = "Ad")
-    soyad = models.CharField(max_lenght = 50, verbose_name = "Soyad")
-    adres = models.CharField(max_lenght = 250, verbose_name = "Adres")
-    telefon = models.CharField(max_lenght = 11, verbose_name = "Telefon")
-    eposta = models.CharField(max_lenght = 50, verbose_name = "E-Posta")
-    tcno = models.CharField(max_lenght = 11, verbose_name = "TC Kimlik Numarası")
-    ehliyet_no = models.CharField(max_lenght = 20, verbose_name = "Ehliyet Numarası")
-    kan_grubu = models.CharField(max_lenght = 10, verbose_name = "Kan Grubu")
-    sgk_no = models.CharField(max_lenght = 250, verbose_name = "SGK Numarası")
-    is_id = models.CharField(max_lenght = 250, verbose_name = "İş ID")
-
-# Belge nesnesini tanımlayan model.
-class Belge(models.Model):
-
+    ad = models.CharField(max_length = 50, verbose_name = "Ad")
+    soyad = models.CharField(max_length = 50, verbose_name = "Soyad")
+    adres = models.CharField(max_length = 250, verbose_name = "Adres")
+    telefon = models.CharField(max_length = 11, verbose_name = "Telefon")
+    eposta = models.CharField(max_length = 50, verbose_name = "E-Posta")
+    tcno = models.CharField(max_length = 11, verbose_name = "TC Kimlik Numarası")
+    ehliyet_no = models.CharField(max_length = 20, verbose_name = "Ehliyet Numarası")
+    kan_grubu = models.CharField(max_length = 10, verbose_name = "Kan Grubu")
+    sgk_no = models.CharField(max_length = 250, verbose_name = "SGK Numarası")
+    is_id = models.ForeignKey(Is, on_delete = models.CASCADE, verbose_name = "İş")
+    aktifmi = models.BooleanField(verbose_name = "Aktif mi?", default = True)
+    olusturma_tarihi = models.DateTimeField(auto_now_add = True, blank = True, verbose_name = "Oluşturma Tarihi")
+    duzunleme_tarihi = models.DateTimeField(verbose_name = "Duzenleme Tarihi", blank = True, null = True)
 
 # BelgeDurum nesnesini tanimlayan model.
 class BelgeDurum(models.Model):
+    durum = models.CharField(max_length = 20, verbose_name = "Durum")
+    aktifmi = models.BooleanField(verbose_name = "Aktif mi?", default = True)
+    olusturma_tarihi = models.DateTimeField(auto_now_add = True, blank = True, verbose_name = "Oluşturma Tarihi")
+    duzunleme_tarihi = models.DateTimeField(verbose_name = "Duzenleme Tarihi", blank = True, null = True)
+
+# Belge nesnesini tanımlayan model.
+class Belge(models.Model):
+    ad = models.CharField(max_length = 50, verbose_name = "Ad")
+    aciklama = models.CharField(max_length = 50, verbose_name = "Açıklama")
+    personel_id = models.ForeignKey(Personel, on_delete = models.CASCADE, verbose_name = "Personel")
+    belge = models.FileField(upload_to='documents/')
+    belge_durum = models.ForeignKey(BelgeDurum, on_delete = models.CASCADE, verbose_name = "Belge Durum")
+    aktifmi = models.BooleanField(verbose_name = "Aktif mi?", default = True)
+    olusturma_tarihi = models.DateTimeField(auto_now_add = True, blank = True, verbose_name = "Oluşturma Tarihi")
+    duzunleme_tarihi = models.DateTimeField(verbose_name = "Duzenleme Tarihi", blank = True, null = True)
 
 
-
+# Eposta nesnesini tanımlayan model.
+class Eposta(models.Model):
+    ad = models.CharField(max_length = 50, verbose_name = "Başlık")
+    mesaj = models.CharField(max_length = 1000, verbose_name = "Mesaj")
+    is_id = models.ForeignKey(Is, on_delete = models.CASCADE, verbose_name = "İş")
+    baglanti_link = models.CharField(max_length = 100, verbose_name = "Bağlantı Linki")
+    aktifmi = models.BooleanField(verbose_name = "Aktif mi?", default = True)
+    olusturma_tarihi = models.DateTimeField(auto_now_add = True, blank = True, verbose_name = "Oluşturma Tarihi")
+    duzunleme_tarihi = models.DateTimeField(verbose_name = "Duzenleme Tarihi", blank = True, null = True)
 
 
 
